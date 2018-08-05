26426f8ea63a753aa6f4ca64c30e10472d5a0bad
SpiritON <satanacamarro@gmail.com>
2018-08-05 21:04:51 +0300

    end html, added java plugins, add responsive

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,82 @@
+//OwlCarouselOption
+$(document).ready(function(){
+
+
+
+
+	  	$(".owl-carousel").owlCarousel({
+
+	loop: true,
+  	margin: 50,
+  	dots: true,
+  	responsiveClass:true,
+		responsive:{
+        320:{
+            items:1,
+            dot: true
+            
+        },
+        768:{
+            items:2,
+            dot: true
+        },
+        1170:{
+            items:3,
+            dot: true
+        },
+
+    }
+
+
+
+				
+	  	
+		});
+
+
+});
+
+
+
+
+
+//SlideToTop
+
 $(function() {
-	
-}); 
+ 
+	$(window).scroll(function() {
+	 
+		if($(this).scrollTop() != 0) 
+
+		{
+		 
+			$('#toTop').fadeIn();
+		 
+		} 
+
+		else 
+
+		{
+		 
+			$('#toTop').fadeOut();
+		}
+		 
+	});
+		 
+	$('#toTop').click(function() {
+		 
+			$('body,html').animate({scrollTop:0},800);
+	 
+	});
+	 
+});
+
+//burgerMenuButton
+$('.burgerMenu').on('click', function(){
+
+	$(this).toggleClass('active')
+
+})
+
+
+ 
